a0b8378f095d1a74f3b9790c0b4ca47dc75862c9
hinakof <hinakof@C010423534.zipangu.cac.co.jp>
2026-04-30 12:36:25 +0900

    0430 q01_basic.question07;まで完了
    理解できた点
    ・引数の Member 配列を拡張 for 文で取得する方法
    
    次回見直す点
    ・強制的にメソッド処理を止めるための return;の記述

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question06/SystemMain06.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question06/SystemMain06.java
@@ -1,5 +1,7 @@
 package q01_basic.question06;
 
+import java.io.IOException;
+
 public class SystemMain06 {
 
 	public static void main(String[] args) {
@@ -12,8 +14,31 @@ public class SystemMain06 {
 		int rank = 2;
 
 		System.out.println("---CREATE MEMBER DATA---");
-		//TODO ここから実装する
 
+		System.out.println("input id>>1");
+		try {
+			inputId = cr.inputNumber();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		} catch (NumberFormatException e) {
+			System.out.println("error");
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println("input password>>");
+		try {
+			inputPassword = cr.inputString();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		Member member = new Member(inputId, inputPassword, name, age, rank);
+
+		System.out.println("---SHOW MEMBER---");
+		member.showMember();
 	}
 
 }
--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question07/NumberList.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question07/NumberList.java
@@ -21,7 +21,10 @@ class NumberList {
 	 * @param limit（Listの最終要素）
 	 */
 	public void addFromOneTo(Integer limit) {
-		//TODO ここから実装する
+		numbers = new ArrayList<>();
+		for (int i = 1; i <= limit; i++) {
+			numbers.add(i);
+		}
 
 	}
 
@@ -32,7 +35,9 @@ class NumberList {
 	 */
 	public Integer calcSumOfList() {
 		Integer sum = 0;
-		//TODO ここから実装する
+		for (Integer n : numbers) {
+			sum += n;
+		}
 
 		return sum;
 	}
@@ -42,7 +47,9 @@ class NumberList {
 	 * 
 	 */
 	public void doubleListEachValue() {
-		//TODO ここから実装する
+		for (int i = 0; i < numbers.size(); i++) {
+			numbers.set(i, numbers.get(i) * 2);
+		}
 	}
 
 	/**
@@ -50,7 +57,11 @@ class NumberList {
 	 * 要素数が奇数の場合、中央値は残す
 	 */
 	public void removeIndexOfFirstHalf() {
-		//TODO ここから実装する
+		int siza = numbers.size();
+		int removeCount = siza / 2;
+		for (int i = 0; i < removeCount; i++) {
+			numbers.remove(0);
+		}
 
 	}
 
